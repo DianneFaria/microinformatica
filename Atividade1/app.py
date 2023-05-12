b5f037548d44907fe265a34f5992fb8b74ee7e33
DianneFaria <diannefaria09@gmail.com>
2023-05-12 16:31:32 -0300

    Criação de banco de dados para cadastro de contato e página de usuários

--- Atividade1/app.py
+++ Atividade1/app.py
@@ -1,18 +1,63 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, url_for
 
-app = Flask("__name__")
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+
+# conexão com o banco de dados
+app.config['MYSQL_Host'] = 'localhost' # 127.0.0.1
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'fatec'
+app.config['MYSQL_DB'] = 'desafio3'
+
+mysql = MySQL(app)
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-""" def hello_world():
-    return "<p>Pedro</p>" """
 
 @app.route("/quemsomos")
 def quemsomos():
     return render_template("quemsomos.html")
 
+'''
 @app.route("/contato")
 def contato():
     return render_template("contato.html")
+'''
+
+@app.route('/contato', methods=['GET', 'POST'])
+def contato():
+    if request.method == 'POST':
+        email = request.form['email']
+        assunto = request.form['assunto']
+        descricao = request.form['descricao']
+
+
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO contato(email, assunto, descricao) VALUES (%s, %s, %s)", (email, assunto, descricao))
+
+
+        mysql.connection.commit()
+
+
+        cur.close()
+
+        return'sucesso'
+    return render_template('contato.html')
+
+
+#rota usuários para listar todos os usuários no banco de dados
+@app.route('/users')
+def users():
+    cur = mysql.connection.cursor()
+
+    users = cur.execute("SELECT * FROM contato")
+
+    if users > 0:
+        userDetails = cur.fetchall()
+
+        return render_template("users.html", userDetails=userDetails)
+    
